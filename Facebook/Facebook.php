79bd852c9dd7c40a1c161e7b43b499b01b8c6c4d
Alan Christian Gonçalves Ferreira <alanchristiangoncalvesferreira@hotmail.com>
2017-12-15 16:28:04 -0200

    Delete Facebook.php

--- Facebook/Facebook.php
+++
@@ -1,8 +0,0 @@
- <ul >
- <li><a href="https://www.facebook.com/AlanChristianGoncalvesFerreira/" target="_blank" ><img src="image/perfil-AlanFacebook.png" /> </a> <a href="#" class="left">
- <div id="bntop"></div></a> </li>
-  <li><a href="https://www.facebook.com/AlanChristianGoncalves/" target="_blank" ><img src="image/perfil-AlanFacebook.png" /> </a> <a href="#" class="left">
- <div id="bntop"></div></a> </li>
-   <li><a href="#" target="_blank" ><img src="image/perfil-AlanInstagram.png" /> </a> <a href="#" class="left">
- <div id="bntop"></div></a> </li>
-    </ul>
