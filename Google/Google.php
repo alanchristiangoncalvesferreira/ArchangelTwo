a817b00525b778fc0705bf918521919e44dd8757
Alan Christian Gonçalves Ferreira <alanchristiangoncalvesferreira@hotmail.com>
2017-12-15 16:32:24 -0200

    Add files via upload

---
+++ Google/Google.php
@@ -0,0 +1,26 @@
+ <ul>
+   <li>
+   <a href="https://plus.google.com/u/0/118063287887635621611" target="_blank" >
+   <img src="image/perfil-AlanGoogle.png" />
+    </a> 
+   <a href="#" class="left"><div id="bntop"></div></a>
+    </li>
+   
+    <li><a href="https://sites.google.com/view/alanchristiangoncalvesferreira/p%C3%A1gina-inicial" target="_blank" >
+    <img src="image/perfil-AlanGoogle.png" /> </a>
+     <a href="#" class="left"><div id="bntop"></div></a> 
+     </li>
+  
+  
+  <li>
+  <a href="    http://alanchristiangoncalvesferreira.blogspot.com.br/" target="_blank" >
+  <img src="image/perfil-AlanGoogle.png" /> </a>
+   <a href="#" class="left"><div id="bntop"></div></a> 
+   </li>
+  
+
+  
+  
+ 
+    </ul>
+    
