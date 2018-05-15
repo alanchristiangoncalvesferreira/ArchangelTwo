f0f1573ece3adb08bc8929dd4e1f4bd7ec5dc848
Alan Christian Gonçalves Ferreira <alanchristiangoncalvesferreira@hotmail.com>
2018-05-15 00:55:12 -0300

    Add files via upload

--- Facebook/Facebook.php
+++ Facebook/Facebook.php
@@ -3,6 +3,6 @@
  <div id="bntop"></div></a> </li>
   <li><a href="https://www.facebook.com/AlanChristianGoncalves/" target="_blank" ><img src="image/perfil-AlanFacebook.png" /> </a> <a href="#" class="left">
  <div id="bntop"></div></a> </li>
-   <li><a href="#" target="_blank" ><img src="image/perfil-AlanInstagram.png" /> </a> <a href="#" class="left">
+   <li><a href="https://www.instagram.com/alanchristiangoncalvesferreira/" target="_blank" ><img src="image/perfil-AlanInstagram.png" /> </a> <a href="#" class="left">
  <div id="bntop"></div></a> </li>
     </ul>
--- partnership.php
+++ partnership.php
@@ -1,9 +1,41 @@
  <div id="partinership">
   <a href="Curriculum.pdf" target="_black"><button class="icon1" ></button></a> 
   
-   
+   <a href="https://www.facebook.com/arianarockefeller/?fref=ts" target="_black"> <button class="icon2" ></button></a>
    <a href="https://www.facebook.com/rockefellercenternyc" target="_black"><button class="icon3" ></button></a>
+    <a href="https://www.facebook.com/OakleyBrasil/" target="_black"><button class="icon4" ></button></a>
+    <a href="https://www.facebook.com/MSFTBrasil/" target="_black"><button class="icon5" ></button></a>
     
- 
- 
+    <a href="https://www.facebook.com/chase/" target="_black"><button class="icon6" ></button></a>
+    <a href="https://www.facebook.com/HospitalAlbertEinstein/" target="_black"><button class="icon7" ></button></a>
+    <a href="https://www.facebook.com/HospitalAlbertEinstein/" target="_black"><button class="icon8" ></button></a>
+    <a href="https://www.facebook.com/AlanChristianGoncalvesFerreira" target="_black"><button class="icon9" ></button></a>
+    <a href="https://www.facebook.com/AlanChristianGoncalvesFerreira" target="_black"><button class="icon10" ></button></a>
+    <a href="https://www.facebook.com/AlanChristianGoncalvesFerreira" target="_black"><button class="icon11" ></button></a>
+
+    <a href="https://www.facebook.com/AlanChristianGoncalvesFerreira" target="_black"><button class="icon12" ></button></a>
+    <a href="https://www.facebook.com/AlanChristianGoncalvesFerreira" target="_black"><button class="icon13" ></button></a>
+   
+   <a href="https://www.facebook.com/AlanChristianGoncalvesFerreira" target="_black"><button class="icon14" ></button></a>
+    <a href="https://www.facebook.com/AlanChristianGoncalvesFerreira" target="_black"><button class="icon15" ></button></a>
+       <a href="https://www.facebook.com/AlanChristianGoncalvesFerreira" target="_black"><button class="icon16" ></button></a>
+    <a href="https://www.facebook.com/AlanChristianGoncalvesFerreira" target="_black"><button class="icon17" ></button></a>
+    <a href="https://www.facebook.com/AlanChristianGoncalvesFerreira" target="_black"><button class="icon18" ></button></a>
+    <a href="https://www.facebook.com/AlanChristianGoncalvesFerreira" target="_black"><button class="icon19" ></button></a>
+    <a href="https://www.facebook.com/AlanChristianGoncalvesFerreira" target="_black"><button class="icon20" ></button></a>
+   <a href="https://www.facebook.com/AlanChristianGoncalvesFerreira" target="_black"><button class="icon21" ></button></a>
+   <a href="https://www.facebook.com/AlanChristianGoncalvesFerreira" target="_black"><button class="icon22" ></button></a>
+    
+   <a href="https://www.facebook.com/AlanChristianGoncalvesFerreira" target="_black"><button class="icon23" ></button></a>
+    <a href="https://www.facebook.com/AlanChristianGoncalvesFerreira" target="_black"><button class="icon24" ></button></a>
+    <a href="https://www.facebook.com/AlanChristianGoncalvesFerreira" target="_black"><button class="icon25" ></button></a>
+    <a href="https://www.facebook.com/AlanChristianGoncalvesFerreira" target="_black"><button class="icon26" ></button></a>
+   <a href="https://www.facebook.com/AlanChristianGoncalvesFerreira" target="_black"><button class="icon27" ></button></a>
+    <a href="https://www.facebook.com/AlanChristianGoncalvesFerreira" target="_black"><button class="icon28" ></button></a>
+    <a href="https://www.facebook.com/AlanChristianGoncalvesFerreira" target="_black"><button class="icon29" ></button></a>
+      <a href="https://www.facebook.com/AlanChristianGoncalvesFerreira" target="_black"><button class="icon30" ></button></a>
+    <a href="https://www.facebook.com/AlanChristianGoncalvesFerreira" target="_black"><button class="icon31" ></button></a>
+    <a href="https://www.facebook.com/AlanChristianGoncalvesFerreira" target="_black"><button class="icon32" ></button></a>
+        <a href="https://www.facebook.com/AlanChristianGoncalvesFerreira" target="_black"><button class="icon33" ></button></a>
+  
   </div>
