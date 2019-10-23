8e9410e3f41342e4f4d1c30fc492c33bbc682504
Alan Christian Gonçalves Ferreira <alanchristiangoncalvesferreira@hotmail.com>
2019-10-23 20:12:26 -0300

    Add files via upload

--- perfil.php
+++ perfil.php
@@ -28,11 +28,13 @@ dataType: 'html',
 </script>
 <style>
 #chamadamenudois{}
+.textobase{color: red;
+    font-family: tahoma;}
 </style>
 <div id="home">
 
 <div id="bloco2">
-<ul><li><a class="icon-print" href="Curriculum.pdf" target="_blank" ></a></li>
+<ul><li><a class="icon-print" href="#"  ></a></li>
 <li ><a id="chamadamenudois"  class="icon-user-6" href="#"></a></li>
 <li ><a  class="icon-graduation-cap-1"></a></li>
 </ul>
@@ -41,13 +43,15 @@ dataType: 'html',
 <img src="image/AlanChristiangoncalvesferreiraperfil.png" class="perfil"/>
 <h1 class="titulo">Alan Christian Gonçalves Ferreira</h1>
 
-<p class="">Meu nome é Alan Christian Gonçalves Ferreira ,sou designer e programador front end desde 2010 quando comecei minha carreira .</br>
-Essa página foi criada como blog pessoal e profissional e esta destinada a oferecer o melhor de mim e do meu trabalho .
+<p class="textobase">Meu nome é Alan Christian Gonçalves Ferreira ,sou designer e programador front end desde 2010 quando comecei minha carreira .</br>
+Essa página foi criada como blog pessoal e profissional.
 </p>
 
 
 </div>
 <div id="conteudo">
+<h1>Sejam bem vindos ao projeto Archangel Two !</h1>
+<p>Esse projeto foi criado para ser uma das minhas referências em projetos de interface gráfica para sistemas.</p>
 </div>
 </div>
 </div>
